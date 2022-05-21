1b92dbc4bb8baef1bcffe9fae38086c4ebb29bf0
alexia <alexiasanzdegaldeano@gmail.com>
2022-05-22 01:05:01 +0200

    Création test lettres déjà jouées

--- pendu_poo_tu/Testsunitaires_pendu_poo/PartieTests.cs
+++ pendu_poo_tu/Testsunitaires_pendu_poo/PartieTests.cs
@@ -40,5 +40,18 @@ namespace Testsunitaires_pendu_poo
             partie.Jouer('b');
             partie.ViesRestantes.Should().Be(5);
         }
+
+        [Fact]
+        public void LettreDejaJouee ()
+        {
+            Mot motChat = new Mot("chat");
+
+            Partie partie = new Partie(motChat);
+            partie.Jouer('a');
+            partie.Jouer('b');
+            partie.LettresJouees.Should().HaveCount(2);
+            partie.LettresJouees.Should().Contains('a');
+            partie.LettresJouees.Should().Contains('b');
+        }
     }
 }
