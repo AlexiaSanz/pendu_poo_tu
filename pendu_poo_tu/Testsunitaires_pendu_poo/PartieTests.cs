133df30a34ebcc5008c6cb8660ce7f180f72ab91
alexia <alexiasanzdegaldeano@gmail.com>
2022-05-22 00:17:21 +0200

    Premier test sur partie

---
+++ pendu_poo_tu/Testsunitaires_pendu_poo/PartieTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+using pendu_poo_tu;
+
+namespace Testsunitaires_pendu_poo
+{
+    
+    public class PartieTests
+    {
+        [Fact]
+
+        public void commencerPartie ()
+        {
+            Mot motChat = new Mot("chat");
+
+            Partie partie = new Partie(motChat);
+            partie.ViesRestantes.Should().Be(6);
+        }
+    }
+}
