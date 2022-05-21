9a5ce60a53cdbb7df431adad44b9fcdc07c58222
alexia <alexiasanzdegaldeano@gmail.com>
2022-05-22 01:30:39 +0200

    Création PartieBuilder

---
+++ pendu_poo_tu/Testsunitaires_pendu_poo/PartieBuilder.cs
@@ -0,0 +1,26 @@
+﻿using pendu_poo_tu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testsunitaires_pendu_poo
+{
+    internal class PartieBuilder
+    {
+        private string _mot = "chat";
+
+        public PartieBuilder AvecMot(string mot)
+        {
+            this._mot = mot;
+            return this;
+        }
+
+        public Partie Build()
+        {
+            var mot = new Mot(this._mot);
+            return new Partie(mot);
+        }
+    }
+}
