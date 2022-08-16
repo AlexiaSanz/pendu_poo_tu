c4d1f4ce4f438e19466db33ad0ee4b9abe8947bd
alexia <alexiasanzdegaldeano@gmail.com>
2022-08-16 17:53:04 +0200

    Première tentative css responsive + Création controlleur partie + Tirage au sort d'un mot

---
+++ pendu_poo_tu/PenduWeb/Controllers/PartieController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pendu_poo_tu;
+using PenduWeb.Data;
+
+namespace PenduWeb.Controllers
+{
+    public class PartieController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PartieController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var mot = MotAuHasard();
+            return View();
+        }
+
+        private pendu_poo_tu.Mot MotAuHasard()
+        {
+            int nb_mots = this._context.Mots.Count();
+
+            Random rnd = new Random();
+            var indexMot = rnd.Next(nb_mots);
+            var motData = this._context.Mots.Skip(indexMot).First();
+            return new pendu_poo_tu.Mot(motData.Contenu);
+        }
+    }
+}
